e192480034c3d9e192844c5b8bf0a980c3cfc188
Felipe Morais <moraisfelipe97@gmail.com>
2018-03-08 10:24:12 -0300

    Started analise-empirica-1

---
+++ ProjetoAnaliseEmpirica1/comandos.cpp
@@ -0,0 +1,1 @@
+g++ -Wall -std=c++11 src/main.cpp src/search.cpp -I include -o bin/exe
---
+++ ProjetoAnaliseEmpirica1/include/search.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <iterator>
+
+long int * linearSearch(long int *, long int *, long int);
+long int * linearSearch(long int *, long int *, long int);
+
+
+#endif
